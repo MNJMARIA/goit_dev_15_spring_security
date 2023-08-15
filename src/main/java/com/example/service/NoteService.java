d0bef262795fdb31fa0547a7489c2809e2d017b8
Maria <stepanovamasha70@gmail.com>
2023-08-15 22:36:22 +0300

    first commit

--- src/main/java/com/example/service/NoteService.java
+++ src/main/java/com/example/service/NoteService.java
@@ -11,32 +11,16 @@ import java.util.*;
 @Service
 public class NoteService implements INoteService {
     private final NoteRepository repository;
-    //private Map<Long, Note> mapOfNotes = new HashMap<>();
 
     @Override
     public Note add(Note note) {
         return repository.save(note);
-        
-        /*// Generate a unique long value using UUID
-        long id = new Random().nextLong();
-        note.setId(id);
-        mapOfNotes.put(note.getId(), note);
-        return note; // Return the added note instead of the return value of put()
-        */
     }
 
     @Override
     public Note getById(long id) {
         Optional<Note> noteOptional = repository.findById(id);
         return noteOptional.orElseThrow(() -> new NoSuchElementException("Note not found!"));
-
-        
-        /*if(mapOfNotes.containsKey(id)){
-            Note note = mapOfNotes.get(id);
-            return note;
-        } else {
-            throw new NullPointerException("Not found!");
-        }*/
     }
 
     @Override
@@ -47,20 +31,10 @@ public class NoteService implements INoteService {
     @Override
     public void update(Note note) {
         repository.save(note);
-        /*if(mapOfNotes.containsKey(note.getId())){
-            mapOfNotes.put(note.getId(), note);
-        } else {
-            throw new NullPointerException("Not found!");
-        }*/
     }
 
     @Override
     public void deleteById(long id) {
         repository.deleteById(id);
-        /*if(mapOfNotes.containsKey(id)){
-            mapOfNotes.remove(id);
-        } else {
-            throw new NullPointerException("Not found!");
-        }*/
     }
 }
