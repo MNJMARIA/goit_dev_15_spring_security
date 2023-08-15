a99157b896252b18bf950e38020ba9f40e47493c
Maria <stepanovamasha70@gmail.com>
2023-08-15 22:17:50 +0300

    first commit

--- src/main/java/com/example/entity/Note.java
+++ src/main/java/com/example/entity/Note.java
@@ -2,9 +2,17 @@ package com.example.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
+@Table(name = "note")
+@Entity
 @Data
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String title;
+
     private String content;
 }
--- src/main/java/com/example/service/NoteService.java
+++ src/main/java/com/example/service/NoteService.java
@@ -1,57 +1,66 @@
 package com.example.service;
 
+import com.example.NoteRepository;
 import com.example.entity.Note;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @RequiredArgsConstructor
-@Data
 @Service
 public class NoteService implements INoteService {
-    private Map<Long, Note> mapOfNotes = new HashMap<>();
+    private final NoteRepository repository;
+    //private Map<Long, Note> mapOfNotes = new HashMap<>();
 
     @Override
     public Note add(Note note) {
-        // Generate a unique long value using UUID
+        return repository.save(note);
+        
+        /*// Generate a unique long value using UUID
         long id = new Random().nextLong();
         note.setId(id);
         mapOfNotes.put(note.getId(), note);
         return note; // Return the added note instead of the return value of put()
+        */
     }
 
     @Override
     public Note getById(long id) {
-        if(mapOfNotes.containsKey(id)){
+        Optional<Note> noteOptional = repository.findById(id);
+        return noteOptional.orElseThrow(() -> new NoSuchElementException("Note not found!"));
+
+        
+        /*if(mapOfNotes.containsKey(id)){
             Note note = mapOfNotes.get(id);
             return note;
         } else {
             throw new NullPointerException("Not found!");
-        }
+        }*/
     }
 
     @Override
     public List<Note> listAll() {
-        return List.copyOf(mapOfNotes.values());
+        return repository.findAll();
     }
 
     @Override
     public void update(Note note) {
-        if(mapOfNotes.containsKey(note.getId())){
+        repository.save(note);
+        /*if(mapOfNotes.containsKey(note.getId())){
             mapOfNotes.put(note.getId(), note);
         } else {
             throw new NullPointerException("Not found!");
-        }
+        }*/
     }
 
     @Override
     public void deleteById(long id) {
-        if(mapOfNotes.containsKey(id)){
+        repository.deleteById(id);
+        /*if(mapOfNotes.containsKey(id)){
             mapOfNotes.remove(id);
         } else {
             throw new NullPointerException("Not found!");
-        }
+        }*/
     }
 }
